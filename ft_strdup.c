003747179df5ba614cfe7cd45166267502b3fc86
Anderson Porto <andersonhigo2@gmail.com>
2021-08-01 01:16:39 -0300

    Create ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/01 01:15:37 by anhigo-s          #+#    #+#             */
+/*   Updated: 2021/08/01 01:15:38 by anhigo-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*string;
+
+	string = malloc(ft_strlen(s1) + 1);
+	if (string == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		ft_memcpy (string, s1, ft_strlen (s1) + 1);
+	}
+	return (string);
+}
