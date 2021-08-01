8deee93f6b35b680507fe5b0eef7157a77cbbead
Anderson Porto <andersonhigo2@gmail.com>
2021-08-01 01:16:15 -0300

    Create ft_calloc.c

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/01 01:03:28 by anhigo-s          #+#    #+#             */
+/*   Updated: 2021/08/01 01:03:29 by anhigo-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	i;
+	void	*pointer;
+
+	i = count * size;
+	pointer = malloc(i);
+	ft_bzero (pointer, i);
+	return (pointer);
+}
