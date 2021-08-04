c8ed58107cd6adf23405cbdc3ef20fb45d8d8a0b
Anderson Porto <andersonhigo2@gmail.com>
2021-08-04 18:42:32 -0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 23:12:34 by anhigo-s          #+#    #+#             */
-/*   Updated: 2021/07/30 23:12:35 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2021/08/04 18:42:15 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	{
 		return ((char *)haystack);
 	}
-	while (len >= ft_strlen(needle))
+	while (len >= ft_strlen(needle) && *haystack != '\0')
 	{
 		if (ft_memcmp(haystack, needle, ft_strlen(needle)) == 0)
 		{
