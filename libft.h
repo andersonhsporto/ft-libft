8eca6faed03d680ff80766d707a4e70a76f135cc
Anderson Porto <andersonhigo2@gmail.com>
2022-04-19 00:50:25 -0300

    style: add comments

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 23:22:37 by anhigo-s          #+#    #+#             */
-/*   Updated: 2022/03/17 13:45:53 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2022/04/19 00:49:54 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -127,6 +127,7 @@ void	ft_putstr_fd(char *s, int fd);
 *	@return void
 */
 void	ft_putendl_fd(char *s, int fd);
+
 /*
 *	@brief Outputs the integer ’n’ (with write(3)) to the given file
 *	descriptor.
@@ -135,7 +136,9 @@ void	ft_putendl_fd(char *s, int fd);
 *	@return void
 */
 void	ft_putnbr_fd(int n, int fd);
+
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
@@ -143,7 +146,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
-size_t	ft_intlen(int n);
+
 size_t	ft_strlen(const char *s);
 /*
 *	@brief Appends string src to the end of dst. It will append at most
@@ -262,5 +265,10 @@ int		ft_strcmp(const char *s1, const char *s2);
 int		ft_isspace(int c);
 char	*ft_strtok(char *input, char d);
 
+/*
+*	@brief get line froam a file descriptor
+*	@param fd The file descriptor.
+*	@return The line read. NULL if the allocation fails.
+*/
 char	*get_next_line(int fd);
 #endif
