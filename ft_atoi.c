cc0aceb7e503185e73ac0730e34785fc92a77e9e
Anderson Porto <anderson.higo2@gmail.com>
2023-01-21 01:22:30 -0300

    refactor: using jump_spaces

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,13 @@
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 14:55:49 by anhigo-s          #+#    #+#             */
-/*   Updated: 2022/01/05 00:00:54 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2023/01/21 01:07:39 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	jump_spaces(int *number, const char *str);
 
 int	ft_atoi(const char *str)
 {
@@ -21,8 +23,7 @@ int	ft_atoi(const char *str)
 	number = 0;
 	sign = 1;
 	i = 0;
-	while (ft_isspace(str[i]))
-		i++;
+	jump_spaces(&i, str);
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i++] == '-')
@@ -32,10 +33,16 @@ int	ft_atoi(const char *str)
 	{
 		number = number * 10 + (str[i] - '0');
 		i++;
-		if (number > 2147483647 && sign == 1)
+		if (number > MAX_INT && sign == POSITIVE)
 			return (-1);
-		if (number > 2147483648 && sign == -1)
+		if (number > MIN_INT && sign == NEGATIVE)
 			return (0);
 	}
 	return (sign * (int)number);
+}
+
+void	jump_spaces(int *number, const char *str)
+{
+	while (ft_isspace(str[*number]))
+		(*number)++;
 }
