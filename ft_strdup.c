b83109e61396fdbe3f62064f89914c10e7c26be7
Anderson Porto <anderson.higo2@gmail.com>
2023-09-09 18:00:30 -0300

    refactor

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 01:15:37 by anhigo-s          #+#    #+#             */
-/*   Updated: 2023/02/23 17:15:20 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2023/09/09 17:42:41 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,13 @@
 char	*ft_strdup(const char *s1)
 {
 	char	*string;
-	size_t	size;
 
 	if (!s1)
 		return (NULL);
 	string = malloc(ft_strlen(s1) + 1);
-	if (!string)
-		return (NULL);
-	size = ft_strlen(s1);
-	ft_memcpy(string, s1, size);
-	string[size] = '\0';
-	return (string);
+	if (string)
+	{
+		return (ft_strcpy(string, s1));
+	}
+	return (NULL);
 }
