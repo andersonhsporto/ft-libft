ebf7781966d2f700c2f542d26e53d1f2426cfef5
Anderson Porto <anderson.higo2@gmail.com>
2023-09-09 17:38:19 -0300

    refactor

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 13:23:05 by anhigo-s          #+#    #+#             */
-/*   Updated: 2023/02/23 17:27:14 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2023/09/09 17:37:27 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,13 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char	*pdest;
-	char	*psrc;
+	const unsigned char	*psrc = (const unsigned char *)src;
+	unsigned char		*pdest;
 
 	if (!dest && !src)
 		return (NULL);
-	pdest = (char *)dest;
-	psrc = (char *)src;
-	while (n)
-	{
-		pdest[n - 1] = psrc[n - 1];
-		n--;
-	}
-	return (pdest);
+	pdest = (unsigned char *)dest;
+	while (n--)
+		*pdest++ = *psrc++;
+	return (dest);
 }
