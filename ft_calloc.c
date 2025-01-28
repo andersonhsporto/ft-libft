194e3753faa6e8517b2f24d9e151c0afb441d694
Anderson Porto <anderson.higo2@gmail.com>
2025-01-28 00:53:02 -0300

    fix: correct calloc implementation and error handling

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,40 +6,40 @@
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 01:03:28 by anhigo-s          #+#    #+#             */
-/*   Updated: 2023/02/23 17:25:30 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2025/01/28 00:48:48 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	is_overflow(size_t count, size_t size);
+static int	is_overflow(size_t count, size_t size)
+{
+	if (count != 0 && size > ((size_t)-1) / count)
+	{
+		errno = ENOMEM;
+		return (1);
+	}
+	return (0);
+}
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*pointer;
 
-	if (count == 0 || size == 0 || is_overflow(count, size))
+	if (count == 0 || size == 0)
+	{
+		pointer = malloc(1);
+		if (pointer)
+			ft_memset(pointer, 0, 1);
+		return (pointer);
+	}
+	if (is_overflow(count, size))
 	{
 		return (NULL);
 	}
 	pointer = malloc(count * size);
 	if (!pointer)
 		return (NULL);
-	return (ft_memset(pointer, 0, (count * size)));
-}
-
-static int	is_overflow(size_t count, size_t size)
-{
-	size_t	result;
-
-	result = count * size;
-	if (count == result / size)
-	{
-		return (0);
-	}
-	else
-	{
-		errno = ENOMEM;
-		return (1);
-	}
+	ft_memset(pointer, 0, (count * size));
+	return (pointer);
 }
